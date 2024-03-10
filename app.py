63455c0bbf4c017e4acaa59ed92cb7a8b81a31c5
savenkovamaria <savenkova_mars@mail.ru>
2024-03-10 17:02:06 +0500

    commit

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from litestar import Litestar
 from litestar.contrib.sqlalchemy.plugins import SQLAlchemyPlugin
+from litestar.contrib.sqlalchemy.base import UUIDBase
+from litestar.di import Provide
 
 from src.endpoints.auth import register_handler, login_handler
 from src.endpoints.roles import assign_role_handler, remove_role_handler
+from src.endpoints.func import GuestsController
 from src.auth import jwt_auth
 from src.db import db_config
-from src.dependencies import provide_transaction
+from src.dependencies import provide_transaction, limitoffsetpagination
+
+
+async def start() -> None:
+    async with db_config.get_engine().begin() as conn:
+        await conn.run_sync(UUIDBase.metadata.create_all)
+
 
 app = Litestar(
-    [register_handler, login_handler, assign_role_handler, remove_role_handler],
+    [register_handler, login_handler, assign_role_handler, remove_role_handler, GuestsController],
     on_app_init=[jwt_auth.on_app_init],
-    dependencies={"transaction": provide_transaction},
+    on_startup=[start],
+    dependencies={"transaction": Provide(provide_transaction), 
+                  "limit_offset": Provide(limitoffsetpagination, sync_to_thread=False)},
     plugins=[SQLAlchemyPlugin(db_config)],
 )
--- src/dependencies.py
+++ src/dependencies.py
@@ -4,6 +4,8 @@ from litestar.exceptions import ClientException
 from litestar.status_codes import HTTP_409_CONFLICT
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from litestar.params import Parameter
+from litestar.repository.filters import LimitOffset
 
 
 async def provide_transaction(db_session: AsyncSession) -> AsyncGenerator[AsyncSession, None]:
@@ -15,3 +17,15 @@ async def provide_transaction(db_session: AsyncSession) -> AsyncGenerator[AsyncS
             status_code=HTTP_409_CONFLICT,
             detail=str(exc),
         ) from exc
+
+
+def limitoffsetpagination(
+    current_page: int = Parameter(ge=1, query="currentPage", default=1, required=False),
+    page_size: int = Parameter(
+        query="pageSize",
+        ge=1,
+        default=10,
+        required=False,
+    ),
+) -> LimitOffset:
+    return LimitOffset(page_size, page_size * (current_page - 1))
