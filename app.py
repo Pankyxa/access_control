2214499692c78bcbbe2e87be5d80cbb840dea501
Pankyxa <114245476+Pankyxa@users.noreply.github.com>
2024-04-08 06:39:52 +0000

    Update app.py
    refactor

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from litestar.config.cors import CORSConfig
 from litestar.contrib.sqlalchemy.base import UUIDBase
 from litestar.contrib.sqlalchemy.plugins import SQLAlchemyPlugin
 from litestar.di import Provide
+
 from src.auth import jwt_auth
 # from src.channels.notifications import notifications_handler
 from src.db import db_config
@@ -12,15 +13,19 @@ from src.dependencies import provide_transaction, limitoffsetpagination
 from src.endpoints.auth import register_handler, login_handler
 from src.endpoints.requests import RequestsController
 from src.endpoints.roles import assign_role_handler, remove_role_handler
+from src.endpoints.users import create_user_handler
 
 
 async def start() -> None:
     async with db_config.get_engine().begin() as conn:
         await conn.run_sync(UUIDBase.metadata.create_all)
+
+
 cors_config = CORSConfig(allow_origins=['*'])
 
 app = Litestar(
-    [register_handler, login_handler, assign_role_handler, remove_role_handler, RequestsController],
+    [register_handler, login_handler, assign_role_handler, remove_role_handler, RequestsController,
+create_user_handler],
     on_app_init=[jwt_auth.on_app_init],
     on_startup=[start],
     dependencies={"transaction": Provide(provide_transaction),
