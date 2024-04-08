af9670db478187e10af550a1530bcdea5557e6dd
Pankyxa <114245476+Pankyxa@users.noreply.github.com>
2024-04-08 06:29:55 +0000

    Update requests.py
    fix conflict

--- src/endpoints/requests.py
+++ src/endpoints/requests.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from enum import Enum
 from typing import Any
 from uuid import UUID, uuid4
@@ -9,8 +10,10 @@ from litestar.controller import Controller
 from litestar.di import Provide
 from litestar.exceptions import HTTPException
 from litestar.pagination import OffsetPagination
+from litestar.params import Parameter
 from litestar.repository.filters import LimitOffset
 from litestar.security.jwt import Token
+from pydantic import TypeAdapter
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
@@ -21,13 +24,56 @@ from src.models.users import Users
 from src.schemas.requests import Requests, RequestsCreate, RequestsReview
 
 
+class GuestsRepository(SQLAlchemyAsyncRepository[RequestsDto]):
+    model_type = RequestsDto
+
+
+async def guestsrepo(db_session: AsyncSession) -> GuestsRepository:
+    return GuestsRepository(session=db_session)
+
+
+async def guestsdetailsrepo(db_session: AsyncSession) -> GuestsRepository:
+    return GuestsRepository(
+        statement=select(RequestsDto).options(selectinload(RequestsDto.invited)),
+        session=db_session,
+    )
+
+
 class StatusEnum(Enum):
-    NEW = 1
-    ACCEPTED = 2
+	@@ -45,36 +26,53 @@ class StatusEnum(Enum):
     REJECTED = 3
 
 
+class GuestsController(Controller):
+    dependencies = {"guests_repo": Provide(guestsrepo)}
 
+    @get(path="/")
+    async def page(self) -> str:
+        return " "
+
+    @get(path="/requests/get")
+    async def list_requests(
+            self,
+            guests_repo: GuestsRepository,
+            limit_offset: LimitOffset,
+    ) -> OffsetPagination[Requests]:
+        results, total = await guests_repo.list_and_count(limit_offset)
+        type_adapter = TypeAdapter(list[RequestsDto])
+        return OffsetPagination[Requests](
+            items=type_adapter.validate_python(results),
+            total=total,
+            limit=limit_offset.limit,
+            offset=limit_offset.offset,
+        )
+
+    @get(path="/requests/{guests_id:uuid}", dependencies={"guests_repo": Provide(guestsdetailsrepo)})
+    async def get_guest(
+            self,
+            guests_repo: GuestsRepository,
+            guests_id: UUID = Parameter(title="Guest ID", ),
+    ) -> Requests:
+        obj = await guests_repo.get(guests_id)
+        return Requests.model_validate(obj)
 
     @post(path="/requests/create")
     async def create_request(
@@ -42,9 +88,9 @@ class StatusEnum(Enum):
             full_name=data.full_name,
             email_address=data.email_address,
             visit_purpose=data.visit_purpose,
-            place_of_visit=data.place_of_visit,
             datetime_of_visit=data.datetime_of_visit,
             appellant_id=request.user.id,
+            datetime=datetime.now(),
             status=StatusEnum.NEW.value,
             confirming_id=None,
         )
@@ -55,7 +101,6 @@ class StatusEnum(Enum):
         # channels.publish({'message': 'New request created, waiting for confirmation'}, 'sec')
         return Response(status_code=202,
                         content={"message": "Request sent to review", "appellant_id": statement.appellant_id})
-
     @post(path="/requests/review")
     async def request_review(
             self,
@@ -73,6 +118,6 @@ class StatusEnum(Enum):
             raise HTTPException(status_code=404, detail="Request not found")
         result.status = data.status
         result.confirming_id = request.user.id
-        # channels.publish({'message': 'Your request has been reviewed'}, 'applicant')
+        channels.publish({'message': 'Your request has been reviewed'}, 'applicant')
         return Response(status_code=202,
                         content={"message": "Request reviewed successfully", "appellant_id": result.appellant_id})
